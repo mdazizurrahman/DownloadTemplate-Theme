3bf3c41f5e6cbc7ec4af1804ea90ecd9ba65a7a9
Md Azizur Rahman <azizsylbd@gmail.com>
2019-10-22 17:13:59 +0600

    DownloadTemplate Them version 1.0.0

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// for Admin Route Group
+Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth','admin']], function (){
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+    Route::resource('tag','TagController');
+
+});
+
+// for Author Route Group
+Route::group(['as'=>'user.','prefix'=>'user','namespace'=>'Author','middleware'=>['auth','user']], function (){
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+});
